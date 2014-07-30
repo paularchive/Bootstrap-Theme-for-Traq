80ddd1952311880ba92a2042febcec7528d5722e
Paul Hobbel <paulhobbel@hotmail.com>
2014-07-30 18:00:06 +0200

    Added default theme

---
+++ bootstrap/error/404.json.php
@@ -0,0 +1,1 @@
+<?php echo API::response(0, array('error' => 404, 'message' => l('errors.404.message', $request))); ?>
---
+++ bootstrap/users/login.api.php
@@ -0,0 +1,1 @@
+<?php echo API::response(0, array('error' => "invalid_api_key")); ?>
