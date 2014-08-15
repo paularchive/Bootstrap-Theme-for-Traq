7d2ce003f898b68ff465415d80ba4f90c851e5cb
Paul Hobbel <paulhobbel@hotmail.com>
2014-08-15 11:29:36 +0200

    Added config file again

---
+++ config/bootstrap.config.php
@@ -0,0 +1,6 @@
+<?php
+/* This is going to be the config file for the bootstrap theme, but I don't know how I'm going to use it */
+# Should we use an inverted navbar?
+$navbar_inverted .= true;
+
+?>
