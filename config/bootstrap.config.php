36126ddf23e9ad6d4b3ede3ac4db96cdaee4a724
Paul Hobbel <paulhobbel@hotmail.com>
2014-08-15 14:17:49 +0200

    The navbar is now looking into the config.ini file

--- config/bootstrap.config.php
+++ config/bootstrap.config.php
@@ -1,6 +1,15 @@
 <?php
 /* This is going to be the config file for the bootstrap theme, but I don't know how I'm going to use it */
+
+
 # Should we use an inverted navbar?
-$navbar_inverted .= true;
+# Nah I'm not going to do it this way:
+/* $config = parse_ini_file( APPPATH . '/views/' . settings('theme') . '/config/config.ini' );
+
+if ($config['navbar'] == "inverse") {
+	$navbar_inverted = true;
+} else {
+	$navbar_inverted = false;
+} */
 
 ?>
